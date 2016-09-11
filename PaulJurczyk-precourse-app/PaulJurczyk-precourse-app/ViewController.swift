f688b02352cf523334f5e7085f55bef41d47f572
Paul Jurczyk <pauljurczykny@gmail.com>
2016-09-11 10:14:21 -0700

    Prework Submission

---
+++ PaulJurczyk-precourse-app/PaulJurczyk-precourse-app/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  PaulJurczyk-precourse-app
+//
+//  Created by Paul Jurczyk on 9/10/16.
+//  Copyright © 2016 Paul Jurczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: Properties
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK Actions
+    
+    @IBAction func selectedImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        
+        
+    }
+    
+}
+
